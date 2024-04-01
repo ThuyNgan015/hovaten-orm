b7834957cfb53e1076184344b0a4b51dca133f00
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-04-01 21:52:34 +0700

    orm-bai45

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,16 +4,55 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use DB;
 
 class PostController extends Controller
 {
     public function index(){
+        echo '<h2>Query Eloquent Model</h2>';
+
         // $allPosts = Post::all();
-        // $post = Post::find(1);
-        $post = new Post;
-        $post->title ='bài viết 3';
-        $post->content ="Nội dung 3";
-        $post->status = 1; 
+
+        // if ($allPosts->count()>0) {
+        //     foreach ($allPosts as $item) {
+        //         echo $item->title.'<br/>';
+        //     }
+        // }
+
+        // $detail = Post::find(1);
         
+        // dd($detail);
+
+        //Sử dụng query build
+        // $activePosts = DB::table('posts')->where('status',  1)->get();
+
+        // $activePosts = Post::where('status',  1)->oderBy('id', 'DESC')->get();
+        
+        // if ($activePosts->count()>0) {
+        //     foreach ($activePosts as $item) {
+        //         echo $item->title.'<br/>';
+        //     }
+        // }
+
+        // $allPosts = Post::all();
+        // $activePosts = $allPosts->reject(function ($post) {
+        //     //dd($post);
+        //     return $post->status==0; //boolean
+        // });
+        // dd($activePosts);
+
+        // Post::chunk(200, function ($posts) {
+        //     foreach ($posts as $post) {
+        //         echo $post->title."<br>";
+        //     }
+
+        //     echo 'Kết thúc chunk <br/>';
+        // });
+
+        $allPosts = Post::cursor();
+
+        foreach ($allPosts as $item) {
+                echo $item->title."<br/>";
+            }
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,13 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    //Quy ước tên table
+    /*
+    Tên Model: Post => table: posts
+    Tên Model: ProductCategory: product_categories
+    */
 
     protected $table = "posts";
     // Quy ước khóa chính, mặc định laravel sẽ lấy field id làm khóa chính.
 
-    // protected $primarykey = 'id';
+    protected $primarykey = 'id';
 
     //public $incrementing = false;
     //protected $keyType = 'string';
-    public $timestamps = false;
+    public $timestamps = true;
+
+    const CREATED_AT = 'create_at';
+    const UPDATED_AT = 'updated_at';
+
+    protected $attributes = [
+        'status' => 0
+    ];
 }
